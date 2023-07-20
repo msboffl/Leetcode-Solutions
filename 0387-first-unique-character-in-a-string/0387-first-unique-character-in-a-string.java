12ed0c501d5884b1ca327d67c67a1fe5a368e5a5
Mareedu Saibabu <53231064+msboffl@users.noreply.github.com>
2023-07-20 16:51:02 +0530

    Time: 8 ms (80.32%), Space: 44.3 MB (56.48%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
@@ -1,18 +1,11 @@
 class Solution {
     public int firstUniqChar(String s) {
-        for(int i = 0; i < s.length(); i++) {
-            boolean check = false;
-            for(int j = 0; j < s.length(); j++) {
-                if((i != j) && (s.charAt(i) == s.charAt(j))) {
-                    check = true;
-                    break;
-                }
-            }
-            if(check == false) {
+        int freq [] = new int[26];
+        for(int i = 0; i < s.length(); i++)
+            freq [s.charAt(i) - 'a'] ++;
+        for(int i = 0; i < s.length(); i++)
+            if(freq [s.charAt(i) - 'a'] == 1)
                 return i;
-            }
-        }
         return -1;
     }
 }
-
