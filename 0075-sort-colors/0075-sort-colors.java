cca904a2c505719c146534ec274695505c89be9d
Mareedu Saibabu <53231064+msboffl@users.noreply.github.com>
2023-07-20 15:15:07 +0530

    Time: 0 ms (100.00%), Space: 41.1 MB (59.10%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,27 +1,24 @@
 class Solution {
     public void sortColors(int[] nums) {
+        // DNF
         int n = nums.length;
-        int count0 = 0, count1 = 0, count2 = 0;
-        
-        for(int i = 0; i < n; i++) {
-            if(nums[i] == 0) {
-                count0++;
-            } else if (nums[i] == 1) {
-                count1++;
+        int low = 0, mid = 0, high = n-1;
+        while(mid <= high) {
+            if(nums[mid] == 0) {
+                int temp = nums[mid];
+                nums[mid] = nums[low];
+                nums[low] = temp;
+                low++;
+                mid++;
+            } else if(nums[mid] == 1) {
+                mid++;
             } else {
-                count2++;
+                int temp = nums[mid];
+                nums[mid] = nums[high];
+                nums[high] = temp;
+                high--;
             }
         }
-        
-        for(int i = 0; i < count0; i++) {
-            nums[i] = 0;
-        }
-        for(int i = count0; i < count0 + count1; i++) {
-            nums[i] = 1;
-        }
-        for(int i = count0 + count1; i < n; i++) {
-            nums[i] = 2;
-        }
-        
     }
 }
+
