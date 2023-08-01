c4e0673de716e801c595c9c041cf4a2a0b80983a
Mareedu Saibabu <53231064+msboffl@users.noreply.github.com>
2023-08-01 08:04:39 +0530

    Time: 1 ms (76.44%), Space: 44.3 MB (95.90%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,35 +1,27 @@
 class Solution {
-    private void markRow(int[][] arr, int i, int m) {
-        for(int j = 0; j < m; j++) {
-            if(arr[i][j] != 0) {
-                arr[i][j] = -10;
-            }
-        }
-    }
-    private void markCol(int[][] arr, int j, int n) {
-        for(int i = 0; i < n; i++) {
-            if(arr[i][j] != 0) {
-                arr[i][j] = -10;
-            }
-        }
-    }
     public void setZeroes(int[][] matrix) {
         int n = matrix.length;
         int m = matrix[0].length;
+        
+        int[] row = new int[n];
+        int[] col = new int[m];
+        
         for(int i = 0; i < n; i++) {
-           for(int j = 0; j < m; j++) {
-               if(matrix[i][j] == 0) {
-                   markRow(matrix, i, m);
-                   markCol(matrix, j, n);
-               }
-           } 
+            for(int j = 0; j < m; j++) {
+                if(matrix[i][j] == 0) {
+                    row[i] = 1;
+                    col[j] = 1;
+                }
+            }
         }
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (matrix[i][j] == -10) {
+
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                if(row[i] == 1 || col[j] == 1) {
                     matrix[i][j] = 0;
                 }
             }
         }
+        // return matrix;
     }
 }
