71aac8c6e9e6fe78a7aecbbca02372150bb0059f
Mareedu Saibabu <53231064+msboffl@users.noreply.github.com>
2023-08-07 17:30:46 +0530

    Time: 0 ms (100.00%), Space: 44.5 MB (46.12%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -2,18 +2,37 @@ class Solution {
     public int[] searchRange(int[] nums, int target) {
         int[] res = {-1, -1};
         int n = nums.length;
-        for(int i = 0; i < n; i++) {
-            if(nums[i] == target) {
-                res[0] = i;
-                break;
+        int left = 0;
+        int right = n - 1;
+
+        // Perform binary search to find the leftmost occurrence of the target
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                res[0] = mid;
+                right = mid - 1; // Continue searching in the left half for the leftmost occurrence
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
             }
         }
-        for(int i = n-1; i >= 0; i--) {
-            if(nums[i] == target) {
-                res[1] = i;
-                break;
+
+        // Perform binary search to find the rightmost occurrence of the target
+        left = 0;
+        right = n - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                res[1] = mid;
+                left = mid + 1; // Continue searching in the right half for the rightmost occurrence
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
             }
         }
+
         return res;
     }
 }
