46f6945e652b1b62d1a5058a935eda2f936513bc
Mareedu Saibabu <53231064+msboffl@users.noreply.github.com>
2023-08-17 23:12:02 +0530

    Time: 23 ms (79.50%), Space: 54.1 MB (78.97%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
@@ -1,8 +1,7 @@
 class Solution {
     public int findKthLargest(int[] nums, int k) {
         Arrays.sort(nums);
-        // Can't sort int[] in descending order in Java;
-        // Sort ascending and then return the kth element from the end
-        return nums[nums.length - k];
+        int n = nums.length;
+        return nums[n-k];
     }
 }
